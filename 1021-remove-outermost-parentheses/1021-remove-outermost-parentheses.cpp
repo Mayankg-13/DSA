c46175ad50a49c5a379725ce3c5a8471374afc5a
Mayankg-13 <mayankgomase@gmail.com>
2026-07-23 22:03:02 +0530

    Time: 0 ms (100%), Space: 8.9 MB (81.59%) - LeetHub

--- 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.cpp
+++ 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.cpp
@@ -1,13 +1,21 @@
 class Solution {
 public:
     string removeOuterParentheses(string s) {
-        int count=0;
-        string ans="";
-        for(int i=0;i<s.length();i++){
-            if(s[i]==')') count--;
-            if(count != 0) ans.push_back(s[i]);
-            if(s[i]=='(') count ++;
+        string ans = "";
+        int depth = 0;
+
+        for (char ch : s) {
+            if (ch == '(') {
+                if (depth > 0)
+                    ans += ch;
+                depth++;
+            } else {
+                depth--;
+                if (depth > 0)
+                    ans += ch;
+            }
         }
+
         return ans;
     }
 };
